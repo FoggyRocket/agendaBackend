a5d666f637d8509d9a68253dff98b806cb723bc1
Oswaldinho <oswalfut_96@hotmail.com>
2018-01-04 14:14:39 -0600

    lol

--- tasks/models.py
+++ tasks/models.py
@@ -3,4 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Task(models.Model):
-	
+	text = models.TextField()
+	expiry = models.DateTimeField(auto_now_add=True)
+	starts = models.DateTimeField(auto_now_add=True)
+	created = models.DateTimeField(auto_now=True)
+
