ce57f5b5fae0731a1df61157a854d2f1d4f3a630
Foggyrocket <dyl.blink.1.8.2@gmail.com>
2018-05-17 18:54:42 -0500

    all day para las tareas

--- tasks/models.py
+++ tasks/models.py
@@ -23,6 +23,7 @@ class Task(models.Model):
 	created = models.DateTimeField(auto_now_add=True,db_index=True)
 	status = models.CharField(max_length=100, choices=STATUS, default='Activo')
 	priority = models.CharField(max_length=100, choices=PRIORITY, blank=True)
+	allDay = models.BooleanField(default=True)
 	user = models.ForeignKey(User, related_name='tasks', on_delete=models.SET_NULL, blank=True, null=True)
 	project = models.ForeignKey(Project, related_name='tasks', on_delete=models.CASCADE,blank=True, null=True)
 	meeting = models.ForeignKey(Meeting, related_name='tasks', on_delete=models.CASCADE,blank=True, null=True)
